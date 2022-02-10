7c461db48674a52af84ed4d572352952e4e8111b
Haocheng Hu <hhu@zynga.com>
2022-02-09 23:43:46 -0500

    update calculator solution file

--- practicals/pra3_calculator_parser/calculatorParser.py
+++ practicals/pra3_calculator_parser/calculatorParser.py
@@ -1,25 +1,54 @@
 #!/usr/bin/env python3
 
 from ply import yacc
+
 from calculatorLexer import calculatorLexer
 from calculatorLexer import tokens
 
-class calculatorParser:
 
+class calculatorParser:
     # Refer to calculatorLexer.py for the tokens which needs to be parsed.
     # Things to consider:
     # - order of operation
     # - ambiguity
     # - parser structure
 
+    precedence = (
+        ('left', 'PLUS', 'MINUS'),
+        ('left', 'TIMES', 'DIVIDE'),
+    )
+
     def p_expr(self, p):
-        '''
+        """
         expr : NUMBER
-        '''
-        #^       ^
-        #p[0]   p[1]
+        """
+        # ^       ^
+        # p[0]   p[1]
 
         p[0] = p[1]
+
+    def p_op(self, p):
+        """
+        expr : expr PLUS expr
+             | expr MINUS expr
+             | expr TIMES expr
+             | expr DIVIDE expr
+        """
+        op = p[2]
+        if op == '+':
+            p[0] = p[1] + p[3]
+        if op == '-':
+            p[0] = p[1] - p[3]
+        if op == '*':
+            p[0] = p[1] * p[3]
+        if op == '/':
+            p[0] = p[1] / p[3]
+
+    def p_bracks(self, p):
+        """
+        expr : LPAREN expr RPAREN
+        """
+        p[0] = p[2]
 
     # Error handling rule
     def p_error(self, p):
@@ -43,6 +72,7 @@ class calculatorParser:
                 continue
             result = self.parser.parse(s)
             print(result)
+
 
 if __name__ == "__main__":
     m = calculatorParser()
