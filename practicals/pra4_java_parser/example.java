75da83f3afe7bfa2091eef92429e87b9e86e095b
Haocheng Hu <hhu@zynga.com>
2022-02-09 23:47:48 -0500

    add pra4 files

---
+++ practicals/pra4_java_parser/example.java
@@ -0,0 +1,15 @@
+class mainClass {
+	public static void main (String[] arg) {
+		int a = 3;
+	}
+}
+
+class someClass {
+	int i;
+
+	public int foo(int a, int b) {
+		int c = 2;
+		a = a * b;
+		return a;
+	}
+}
